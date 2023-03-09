0b421562a78adc8efa00292b99d2cce6564b262f
Veronica Maxian <maxianveronica20@stud.ase.ro>
2023-03-09 22:46:49 +0200

    Code Update using S.O.L.I.D

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,42 @@
 package main;
 
+import classes.ConnectionImpl;
+import classes.CreateTable;
+import classes.InsertData;
+import classes.ReadData;
+import interfaces.ConnectionDB;
+import interfaces.CreateTableDB;
+import interfaces.InsertDataDB;
+import interfaces.ReadDataDB;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConnectionDB connectionDB=new ConnectionImpl();
+        Connection connection=connectionDB.connectionImpl();
+
+        CreateTableDB createTableDB=new CreateTable();
+        try {
+            createTableDB.createTable(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        InsertDataDB insertDataDB=new InsertData();
+        try {
+            insertDataDB.insertData(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ReadDataDB readDataDB=new ReadData();
+        try {
+            readDataDB.readData(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
